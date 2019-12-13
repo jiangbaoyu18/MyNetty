c4dc76dc3561f52c597b7fd1e63193d66c059635
jiangbaoyu18 <jiangbaoyu18@mails.ucas.ac.cn>
2019-12-13 10:06:33 +0800

    test git reset2

--- src/main/java/com/jby/nio8/Test1.java
+++ src/main/java/com/jby/nio8/Test1.java
@@ -10,7 +10,7 @@ import java.security.SecureRandom;
  * Buffer 本身为一块内存，数据的读写都是通过buffer实现的,基本类型都有对应的Buffer类型
  *      position:下一次要读或写的索引的位置；limit: 第一个不能读或写的索引位置
  *
- * Channel 类似于Stream，可以向其write or  read 数据 （通过Buffer）,Channel是双向的而Stream是单向的
+ * Channel 类似于Stream，可以向其write or  read 数据 （通过Buffer）,Channel是双向的而Stream是单向的 test
  */
 public class Test1 {
 
